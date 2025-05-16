b918ff1dcc725a1c5c7a96ae487a8da37d0019a9
Jeremiah Ondrasik <technologyfrog89@gmail.com>
2025-05-16 16:04:06 -0400

    Autherization (login/out) finished

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # API-V1.0
 My API
+
+https://www.youtube.com/watch?v=g566eI2EmeY&ab_channel=EricRoby
+
+35 minutes
---
+++ fastapi/api/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Boolean
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
